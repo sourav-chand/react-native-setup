2867138f8b3daa01cfcbfa172104128cd8c6e049
souravchand04 <souravchand1109@gmail.com>
2026-05-14 17:27:39 +0530

    configured the nativewind

--- jest.config.js
+++ jest.config.js
@@ -1,3 +1,6 @@
 module.exports = {
   preset: '@react-native/jest-preset',
+  transformIgnorePatterns: [
+    'node_modules/(?!((jest-)?react-native|@react-native(-community)?|nativewind|react-native-css-interop|react-native-reanimated|react-native-worklets)/)',
+  ],
 };
